b701feef0faea2fd39d5e6086a7ea6af961375c2
balajipravin08 <balajipravin12@gmail.com>
2024-11-04 20:08:13 +0530

    add

--- Leet code/P5.java
+++ Leet code/P5.java
@@ -1,14 +1,10 @@
-import java.util.*;
-class P5 {
-	public static boolean check(String s){
-		for(String s1 : s.split("\\.")){
-    			if((s1.charAt(0)=='0' && s1.length() >1) || (Integer.parseInt(s1) > 255 || s1.length()>3  )) return false;
-    		}
-    			return true;
-	}
+public class P5 {
+    public static int findTheWinner(int n, int k) {
+        if(n==1) return 0;
+        return (findTheWinner(n-1,k)+k)%n;
+		}
     public static void main(String[] args) {
-    	String s = "100.01.1.1";
-    	System.out.println(check(s));
+    	System.out.println(findTheWinner(5,2)+1);
     	
     }
 }
