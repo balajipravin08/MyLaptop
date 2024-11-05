a49a1a361332e7611ce580954aa9620470dccbfb
balajipravin08 <balajipravin12@gmail.com>
2024-11-05 11:32:26 +0530

    add

---
+++ Leet code/MaxSubArray.java
@@ -0,0 +1,31 @@
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int[] a = {-11, 11, 15, -1, 30,-2};
+
+        int l=0,r=0,sum=0,max=0,s=0,e=0;
+
+        while (r!=a.length) {
+            if (a[r]<0) {
+                if (sum>max) {
+                    max = sum;
+                    s=l;
+                    e=r; 
+                }
+                sum = 0;
+                l=r+1;
+                r++;
+                continue;
+            }
+            sum += a[r];
+            if (sum>max) {
+                max = sum;
+                s=l;
+                e=r;
+            }
+            r++;
+        }
+        //max = Math.max(max, sum);
+        System.out.println(max+" "+s+" "+e);
+    }
+}
